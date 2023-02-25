682260906f80e33f1fb1c30df27033eefa7f9aee
Luigi <ledragon0804@hotmail.fr>
2023-02-25 11:03:32 +0100

    en cours

--- 4D_test/4D_test/main.cpp
+++ 4D_test/4D_test/main.cpp
@@ -7,13 +7,46 @@ using namespace std;
 int main()
 {
     string chaine;
-    int i;
+    string res = "true";
+    int i, j;
 
     cout << "Bonjour !" << endl;
     cout << "Entrez la chaine de caracteres a examiner." << endl;
     cin >> chaine;
 
-    for (i = 0; i < chaine.size(); i++) {
-        cout << chaine.at(i) << endl;
+    // Vérification des accolades de début et fin
+    if (chaine.at(0) != *"{" || chaine.at(chaine.size() - 1) != *"}")
+        res = "false";
+    else {
+
+        for (i = 1; i < chaine.size(); i++) 
+        {
+            // Vérification si une string
+            if (chaine.at(i) == *"\"")
+            {
+                //Lecture de la string tant qu'on ne détecte pas des guillemets ou fin de json
+                j = i + 1;
+                while (j < chaine.size() && (chaine.at(j) != *"\"" || chaine.at(j) != *"}"))
+                {
+                    cout << j << endl;
+                    cout << chaine.at(j) << endl;
+                    j++;
+                }
+
+                if (chaine.at(j-2) != *"\"")
+                {
+                    res = "false";
+                    break;
+                }
+
+                i = j;
+            }
+
+            cout << "flag" << endl;
+        }
     }
+
+    cout << res << endl;
+
+    return EXIT_SUCCESS;
 }
