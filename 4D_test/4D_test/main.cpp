2a1b728c6d3f30358f7b5a39aaec552866a5288a
Luigi <ledragon0804@hotmail.fr>
2023-02-25 13:47:28 +0100

    Fin projet

--- 4D_test/4D_test/main.cpp
+++ 4D_test/4D_test/main.cpp
@@ -15,7 +15,6 @@ int main()
     cout << "Bonjour !" << endl;
     cout << "Entrez la chaine de caracteres a examiner." << endl;
     cin >> chaine;
-    //chaine = "{\"test\": \"tes\"}";
 
     // Vérification des accolades de début et fin
     if (chaine.at(0) != *"{" || chaine.at(chaine.size() - 1) != *"}")
@@ -24,21 +23,15 @@ int main()
 
         for (i = 1; i < chaine.size(); i++) 
         {
-            if (chaine.at(i) == *"}")
-                break;
+            if (chaine.at(i) == *"}") break;
 
             // Vérification conformité si la value est une string ou number
-            cout << chaine.at(i) << endl;
             if (chaine.at(i) == *"\"")
             {
                 //Lecture de la string tant qu'on ne détecte pas des guillemets ou fin de json
                 j = i + 1;
                 while (chaine.at(j) != *"\"" && chaine.at(j) != *"}")
-                {
-                    cout << j << endl;
-                    cout << "chaine: " << chaine.at(j) << endl;
                     j++;
-                }
 
                 if (chaine.at(j++) != *"\"" || chaine.at(j++) != *":")
                 {
@@ -49,7 +42,7 @@ int main()
                 //Vérification si tableau
                 if (chaine.at(j) == *"[")
                 {
-                    j += 2;
+                    j++;
 
                     int retflag;
                     ArrayAnalyse(chaine, j, res, retflag);
@@ -69,8 +62,6 @@ int main()
                 res = "false";
                 break;
             }
-
-            cout << "flag" << endl;
         }
     }
 
@@ -81,51 +72,59 @@ int main()
 
 void ArrayAnalyse(std::string& chaine, int& j, std::string& res, int& retflag)
 {
-    retflag = 1;
-    while (chaine.at(j) != *"\"" && chaine.at(j) != *"}")
-    {
-        cout << j << endl;
-        cout << "chaine: " << chaine.at(j) << endl;
+    bool obj = false;
+
+    do {
+        if (chaine.at(j) == *"{")
+        {
+            j += 1;
+            obj = true;
+        }
+
         j++;
-    }
-
-    if (chaine.at(j++) != *"\"" || chaine.at(j++) != *":")
-    {
-        res = "false";
-        { retflag = 2; return; };
-    }
-
-    //Vérification value
-    //Cas d'une string
-    if (chaine.at(j++) == *"\"")
-    {
+        retflag = 1;
         while (chaine.at(j) != *"\"" && chaine.at(j) != *"}")
             j++;
 
-        if (chaine.at(j) != *"\"")
+        if (chaine.at(j++) != *"\"" || chaine.at(j++) != *":")
         {
             res = "false";
             { retflag = 2; return; };
         }
 
-        if (chaine.at(++j) != *"]")
+        //Vérification value
+        //Cas d'une string
+        if (chaine.at(j) == *"\"")
         {
-            res = "false";
-            { retflag = 2; return; };
-        }
-    }
-    //Cas d'un chiffre
-    else if (chaine[j] > 47 && chaine[j] < 58)
-    {
-        while (chaine[j] > 47 && chaine[j] < 58)
             j++;
+            while (chaine.at(j) != *"\"" && chaine.at(j) != *"}")
+                j++;
 
-        if (chaine.at(++j) != *"]")
+            if (chaine.at(j) != *"\"")
+            {
+                res = "false";
+                { retflag = 2; return; };
+            }
+            j++;
+        }
+        //Cas d'un chiffre
+        else if (chaine[j] > 47 && chaine[j] < 58)
+        {
+            while (chaine[j] > 47 && chaine[j] < 58)
+                j++;
+        }
+
+        if (obj && chaine.at(j++) != *"}")
+        {
+            retflag = 2; return;
+        };
+
+        if (chaine.at(j) != *"]" && chaine.at(j) != *",")
         {
             res = "false";
             { retflag = 2; return; };
         }
-    }
+    } while (chaine.at(j++) == *",");
 }
 
 void StringOrNumberAnalyse(std::string& chaine, int& j, std::string& res, int& retflag)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 4D-eval-technique
+
+Voici les chaînes de caractères avec lesquelles j'ai efectué mes tests.
+
+{"test":"test","t":4}
+
+{"test":"test","t":4,"a":["fe":"f"]}
+
+{"test":"test","t":4,"a":[{"fe":"f"}]}
+
+{"test":"test","t":4,"a":["fe":"f",{"fe":4}]}
